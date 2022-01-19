1d978a966776e198dfd4fa8d6f118157ea6bdc75
James Gould <jamesgould1994@gmail.com>
2022-01-19 15:23:44 +0000

    new post

--- _site/assets/js/categories.js
+++ _site/assets/js/categories.js
@@ -1,4 +1,4 @@
-const categories = { jekyll: [{ url: `/posts/test/`, date: `04 Nov 2021`, title: `Test markdown file update`},{ url: `/posts/test-Copy/`, date: `04 Nov 2021`, title: `aadditional post`},{ url: `/posts/jekyll-markdown/`, date: `04 Nov 2021`, title: `Jekyll Markdown`},], }
+const categories = { jekyll: [{ url: `/posts/jekyll-markdown/`, date: `04 Nov 2021`, title: `Jekyll Markdown`},], }
 
 window.onload = function () {
   document.querySelectorAll(".category").forEach((category) => {
